8760b450f33e762b99bb2429559c8b3b3c7f41a7
Hristian Iliev <hristian00i@abv.bg>
2018-08-18 17:15:15 +0300

    Create Jedi Meditation.java

---
+++ Jedi Meditation.java
@@ -0,0 +1,40 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Jedi Meditation {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        String[] jedi = new String[N];
+        String line = in.nextLine();
+        String line2 = in.nextLine();
+        String[] jediTokens = line2.split(" ");
+        Queue<String> queueMasters = new LinkedList<>();
+        Queue<String> queueKnights = new LinkedList<>();
+        Queue<String> queuePadwans = new LinkedList<>();
+        for (int i = 0; i < jedi.length; i++) {
+            jedi[i] = jediTokens[i];
+            if(jedi[i].contains("M")){
+                queueMasters.offer(jedi[i]);
+            }else if(jedi[i].contains("K")){
+                queueKnights.offer(jedi[i]);
+            }else if(jedi[i].contains("P")){
+                queuePadwans.offer(jedi[i]);
+            }
+        }
+
+        while(queueMasters.peek() != null){
+            System.out.print(queueMasters.poll() + " ");
+        }
+
+        while(queueKnights.peek() != null){
+            System.out.print(queueKnights.poll() + " ");
+        }
+
+        while(queuePadwans.peek() != null){
+            System.out.print(queuePadwans.poll() + " ");
+        }
+    }
+}
